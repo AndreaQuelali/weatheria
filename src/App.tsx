b37cb4d4338221d19863646d9b2ffe18d722f7c2
AndreaQuelali <andrealizbethquelali5@gmail.com>
2026-02-09 17:47:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+
+    <div className="min-h-screen flex items-center justify-center bg-sky-100">
+      <h1 className="text-4xl font-bold text-sky-800">
+        Weatheria
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
