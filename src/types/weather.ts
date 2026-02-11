03db8ab919b82bf38587668384afca9a9b8d3615
AndreaQuelali <andrealizbethquelali5@gmail.com>
2026-02-11 17:56:48 -0400

    feat: add humidity, feelsLike and windDirection in CurrentWeather

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -2,6 +2,9 @@ export interface CurrentWeather {
     cityName: string;
     temperature: number;
     windSpeed: number;
+    windDirection: number;
+    humidity: number;
+    feelsLike: number;
     weatherCode: number;
     description: string;
 }
